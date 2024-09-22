0f00b1e7a2cc24c46c8cb077316b51ca3c132afc
Songpol Rungsawang <songpol.rungsawang@gmail.com>
2024-09-22 20:44:53 +0700

    feat: add networkBoundResource.

---
+++ core/common/src/main/kotlin/com/songpol/coinranking/core/common/data/NetworkBoundResource.kt
@@ -0,0 +1,35 @@
+package com.songpol.coinranking.core.common.data
+
+import com.songpol.coinranking.core.common.result.Result
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline onFetchFailed: (Throwable) -> Unit = { },
+    crossinline shouldFetch: (ResultType) -> Boolean = { true }
+) = flow {
+    emit(Result.Loading)
+    val data = query().first()
+
+    val flow = if (shouldFetch(data)) {
+        emit(Result.Loading)
+
+        try {
+            saveFetchResult(fetch())
+            query().map { Result.Success(it) }
+        } catch (throwable: Throwable) {
+            onFetchFailed(throwable)
+            query().map { Result.Error(throwable) }
+        }
+    } else {
+        query().map { Result.Success(it) }
+    }
+
+    emitAll(flow)
+}
