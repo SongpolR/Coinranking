5130015e5563d63d174067362c4aaa0439e046f7
Songpol Rungsawang <songpol.rungsawang@gmail.com>
2024-09-23 14:54:45 +0700

    feat: add CoinNetworkDataSourceTest file.

---
+++ core/network/src/test/kotlin/com/songpol/coinranking/core/network/datasource/coin/CoinNetworkDataSourceTest.kt
@@ -0,0 +1,10 @@
+package com.songpol.coinranking.core.network.datasource.coin
+
+import kotlinx.coroutines.test.StandardTestDispatcher
+
+class CoinNetworkDataSourceTest {
+
+    private lateinit var subject: CoinNetworkDataSource
+
+    private val testDispatcher = StandardTestDispatcher()
+}
