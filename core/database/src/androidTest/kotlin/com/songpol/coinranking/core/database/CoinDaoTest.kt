4ed0fbfcb6cfdd90848dad52cce78a277b412d09
Songpol Rungsawang <songpol.rungsawang@gmail.com>
2024-09-26 10:30:33 +0700

    feat: add coin dao test file.

---
+++ core/database/src/androidTest/kotlin/com/songpol/coinranking/core/database/CoinDaoTest.kt
@@ -0,0 +1,8 @@
+import com.songpol.coinranking.core.database.CoinrankingDatabase
+import com.songpol.coinranking.core.database.dao.CoinDao
+
+class CoinDaoTest {
+
+    private lateinit var coinDao: CoinDao
+    private lateinit var databse: CoinrankingDatabase
+}
